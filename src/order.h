b09fd3187ad2aa3f1e88a72df2f119fe7dc107ed
ConnorUsaty <connorusaty@gmail.com>
2025-07-06 22:44:28 -0700

    feat: add constructors

--- src/order.h
+++ src/order.h
@@ -3,20 +3,26 @@
 #include <cstdint>
 
 using OrderId = uint64_t;
-using Price = int64_t; // store price in cents as int
+using Price = int64_t;  // store price in cents as int
 using Quantity = uint32_t;
 using Timestamp = uint64_t;
 
-enum class Side : uint8_t {
-    Buy,
-    Sell
-};
+enum class Side : uint8_t { Buy, Sell };
 
 // each order must have the following data
 struct Order {
-    OrderId id;
-    Side side;
-    Price price;
-    Quantity quantity;
-    Timestamp timestamp;
+  OrderId id;
+  Side side;
+  Price price;
+  Quantity quantity;
+  Timestamp timestamp;
+
+  Order() = default;
+  Order(OrderId _id, Side _side, Price _price, Quantity _quantity,
+        Timestamp _timestamp)
+      : id(_id),
+        side(_side),
+        price(_price),
+        quantity(_quantity),
+        timestamp(_timestamp) {}
 };
