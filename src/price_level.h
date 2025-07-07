0af8a2498cf34f0a958efb9dcf6e2964903ce280
ConnorUsaty <connorusaty@gmail.com>
2025-07-06 18:25:21 -0700

    feat: initial PriceLevel struct

---
+++ src/price_level.h
@@ -0,0 +1,10 @@
+/* A price level is a collection of orders at the same price */
+
+#include <deque>
+#include "order.h"
+
+struct PriceLevel {
+    std::deque<Order*> orders; // process orders in FIFO
+    Price level;
+    Quantity total_quantity; // sum(all orders.quantity on this level)
+};
