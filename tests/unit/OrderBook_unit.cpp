397c1b8461426165179f697ab9245dc7168185f2
ConnorUsaty <connorusaty@gmail.com>
2025-07-06 23:36:11 -0700

    tests: add more tests for add_order()

--- tests/unit/OrderBook_unit.cpp
+++ tests/unit/OrderBook_unit.cpp
@@ -8,31 +8,78 @@ class OrderBookUnitTest : public ::testing::Test {
   void TearDown() override {}
 };
 
-TEST_F(OrderBookUnitTest, AddOrder) {
+TEST_F(OrderBookUnitTest, AddOrderFullMatch) {
   OrderBook book;
 
-  Order ord1(5, Side::Buy, 105, 4, 1);
+  Order ord1(1, Side::Buy, 105, 4, 1);
   book.add_order(ord1);
+  auto& acc_ord1 = book.order_pool[1];
 
+  EXPECT_EQ(acc_ord1, ord1);
   EXPECT_EQ(book.order_pool.size(), 1);
   EXPECT_EQ(book.order_level.size(), 1);
   EXPECT_EQ(book.bids.size(), 1);
   EXPECT_EQ(book.asks.size(), 0);
 
-  Order ord2(6, Side::Buy, 105, 4, 2);
+  Order ord2(2, Side::Buy, 105, 4, 2);
   book.add_order(ord2);
+  auto& acc_ord2 = book.order_pool[2];
 
+  EXPECT_EQ(acc_ord2, ord2);
   EXPECT_EQ(book.order_pool.size(), 2);
   EXPECT_EQ(book.order_level.size(), 2);
   EXPECT_EQ(book.bids.size(), 1);
   EXPECT_EQ(book.asks.size(), 0);
 
-  Order ord3(7, Side::Sell, 105, 10, 3);
+  // full match + extra
+  // covers the 8 buy orders and leaves 2 extra
+  Order ord3(3, Side::Sell, 105, 10, 3);
   book.add_order(ord3);
+  auto& acc_ord3 = book.order_pool[3];
+
+  EXPECT_EQ(acc_ord3, ord3);
+  EXPECT_EQ(book.order_pool.size(), 1);
+  EXPECT_EQ(book.order_level.size(), 1);
+  EXPECT_EQ(book.bids.size(), 0);
+  EXPECT_EQ(book.asks.size(), 1);
+}
+
+TEST_F(OrderBookUnitTest, AddOrderPartialMatch) {
+  OrderBook book;
+
+  // sell 10 @ 100
+  Order sell(1, Side::Sell, 100, 10, 1);
+  book.add_order(sell);
+
+  // buy 6 @ 100 -> partial match
+  Order buy(2, Side::Buy, 100, 6, 2);
+  book.add_order(buy);
 
   EXPECT_EQ(book.order_pool.size(), 1);
   EXPECT_EQ(book.order_level.size(), 1);
   EXPECT_EQ(book.bids.size(), 0);
+  EXPECT_EQ(book.asks.size(), 1);
+
+  auto& remaining_order = book.order_pool.begin()->second;
+  EXPECT_EQ(remaining_order.id, 1);
+  EXPECT_EQ(remaining_order.side, Side::Sell);
+  EXPECT_EQ(remaining_order.price, 100);
+  EXPECT_EQ(remaining_order.quantity, 4);
+  EXPECT_EQ(remaining_order.timestamp, 1);
+}
+
+TEST_F(OrderBookUnitTest, AddOrderNoMatch) {
+  OrderBook book;
+
+  // Buy at 100, Sell at 102 -> no match
+  Order buy(1, Side::Buy, 100, 10, 1);
+  Order sell(2, Side::Sell, 102, 10, 2);
+
+  book.add_order(buy);
+  book.add_order(sell);
+
+  EXPECT_EQ(book.order_pool.size(), 2);
+  EXPECT_EQ(book.bids.size(), 1);
   EXPECT_EQ(book.asks.size(), 1);
 }
 
